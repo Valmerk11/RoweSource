c0db53268149451a91bfc3eb003e8012efe06910
Valmerk11 <162017268+Valmerk11@users.noreply.github.com>
2024-08-01 00:03:39 +0200

    Update level_up_learnsets.h

--- src/data/pokemon/level_up_learnsets.h
+++ src/data/pokemon/level_up_learnsets.h
@@ -3639,6 +3639,9 @@ static const struct LevelUpMove sLaprasLevelUpLearnset[] = {
 
 static const struct LevelUpMove sDittoLevelUpLearnset[] = {
     LEVEL_UP_MOVE( 1, MOVE_TRANSFORM),
+    LEVEL_UP_MOVE( 1, MOVE_RECOVER),
+    LEVEL_UP_MOVE( 1, MOVE_METRONOME),
+    LEVEL_UP_MOVE( 1, MOVE_MIMIC),
     LEVEL_UP_END
 };
 
