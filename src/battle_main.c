83bc61fbace3201a8d1af285ae77e045570f5027
Valmerk11 <162017268+Valmerk11@users.noreply.github.com>
2024-08-31 14:50:53 +0200

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -1878,8 +1878,8 @@ static u8 CreateNPCTrainerParty(struct Pokemon *party, u16 trainerNum, bool8 fir
 	else
 		isDoubleBattle = FALSE;
 	
-	if(IsHardMode() == 1 && PartySize > LeaderMonsCount && trainerNum != TRAINER_TATE_AND_LIZA_1)
-		LeaderMonsCount = PartySize;
+	//if(IsHardMode() == 1 && PartySize > LeaderMonsCount && trainerNum != TRAINER_TATE_AND_LIZA_1)
+		//LeaderMonsCount = PartySize;
 	
     if (trainerNum == TRAINER_SECRET_BASE)
         return 0;
